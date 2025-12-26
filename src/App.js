4214f76a479d7eb998317b491bab3789c0203134
BhanuPrakash056 <bhanu.prakash@network.elancoah.com>
2025-12-26 14:39:27 +0530

    feat: pass delete item handler to PackingList component

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ export default function App() {
     <div className="app">
       <Logo />
       <Form onAddItem={handleAddItem} />
-      <PackingList items={items} />
+      <PackingList items={items} onDeleteItem={handleDeleteItem} />
       <Stats />
     </div>
   );
