ba48440a3bf9966753fc72cc6f2f27046dca4567
BhanuPrakash056 <bhanu.prakash@network.elancoah.com>
2025-12-28 08:41:27 +0530

    feat: add StarRating component for user rating functionality

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import StarRating from './component /Star';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating maxStars={10} />
   </React.StrictMode>
 );
 
